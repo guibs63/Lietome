39e47bcfc5ddd415e80e27b9d3e1413ed285ae20
Guillaume Daudet <guibs.dd@gmail.com>
2026-02-24 17:40:56 +0100

    Fix root route

--- server.js
+++ server.js
@@ -1,31 +1,24 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const OpenAI = require("openai");
 
 const app = express();
 const server = http.createServer(app);
-
 const io = new Server(server);
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
-app.get("/ping", (req, res) => {
-  res.send("pong");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
   console.log("Socket connected:", socket.id);
 
-  socket.on("chatMessage", async (data) => {
-    console.log("Message reçu:", data);
-
-    io.emit("chatMessage", {
-      username: data.username,
-      message: data.message,
-    });
+  socket.on("chatMessage", (data) => {
+    io.emit("chatMessage", data);
   });
 });
 
